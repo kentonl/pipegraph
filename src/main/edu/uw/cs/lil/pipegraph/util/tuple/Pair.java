030779d822e5b0cc445ab6e9165574295d3f1c3c
Kenton Lee <kentonl@cs.washington.edu>
2015-10-11 21:22:47 -0700

    Tuples.

---
+++ src/main/edu/uw/cs/lil/pipegraph/util/tuple/Pair.java
@@ -0,0 +1,46 @@
+package edu.uw.cs.lil.pipegraph.util.tuple;
+
+import java.util.Objects;
+
+public class Pair<A, B> {
+	private final A	first;
+	private final B	second;
+
+	private Pair(A first, B second) {
+		this.first = first;
+		this.second = second;
+	}
+
+	public static <A, B> Pair<A, B> of(A first, B second) {
+		return new Pair<>(first, second);
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other instanceof Pair) {
+			return Objects.equals(this.first, ((Pair<?, ?>) other).first)
+					&& Objects.equals(this.second, ((Pair<?, ?>) other).second);
+		} else {
+			return false;
+		}
+	}
+
+	public A first() {
+		return first;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second);
+	}
+
+	public B second() {
+		return second;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + first + "," + second + ")";
+	}
+
+}
---
+++ src/main/edu/uw/cs/lil/pipegraph/util/tuple/Triplet.java
@@ -0,0 +1,55 @@
+package edu.uw.cs.lil.pipegraph.util.tuple;
+
+import java.util.Objects;
+
+public class Triplet<A, B, C> {
+	private final A	first;
+	private final B	second;
+	private final C	third;
+
+	private Triplet(A first, B second, C third) {
+		this.first = first;
+		this.second = second;
+		this.third = third;
+	}
+
+	public static <A, B, C> Triplet<A, B, C> of(A first, B second, C third) {
+		return new Triplet<>(first, second, third);
+	}
+
+	@Override
+	public boolean equals(Object other) {
+		if (other instanceof Triplet) {
+			return Objects.equals(this.first, ((Triplet<?, ?, ?>) other).first)
+					&& Objects.equals(this.second,
+							((Triplet<?, ?, ?>) other).second)
+					&& Objects.equals(this.third,
+							((Triplet<?, ?, ?>) other).third);
+		} else {
+			return false;
+		}
+	}
+
+	public A first() {
+		return first;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second, third);
+	}
+
+	public B second() {
+		return second;
+	}
+
+	public C third() {
+		return third;
+	}
+
+	@Override
+	public String toString() {
+		return "(" + first + "," + second + "," + third + ")";
+	}
+
+}

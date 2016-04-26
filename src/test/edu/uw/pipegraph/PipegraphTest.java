694dff168000f5a9d6bcf93364e97740189c72d8
Kenton Lee <kentonl@cs.washington.edu>
2016-04-26 13:03:02 -0700

    Add tests for includes.

--- src/test/edu/uw/pipegraph/PipegraphTest.java
+++ src/test/edu/uw/pipegraph/PipegraphTest.java
@@ -1,30 +1,43 @@
 package edu.uw.pipegraph;
 
+import junit.framework.Assert;
+
+import org.junit.Test;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Optional;
 
-import org.junit.Test;
-
 import edu.uw.pipegraph.CommonProto.IntegerResource;
 import edu.uw.pipegraph.core.Pipegraph;
 import edu.uw.pipegraph.runner.AsynchronousPipegraphRunner;
 import edu.uw.pipegraph.runner.IPipegraphRunner;
-import junit.framework.Assert;
 
 public class PipegraphTest {
 	@Test
 	public void arithmeticTest() {
+		doTest(new File("src/test/resources/test.conf"), "final");
+	}
+
+	@Test
+	public void includeTest() {
+		doTest(new File("src/test/resources/test_include.conf"), "test.final");
+	}
+
+	@Test
+	public void nestedIncludeTest() {
+		doTest(new File("src/test/resources/test_nested_include.conf"), "test_include.test.final");
+	}
+
+	private void doTest(final File configFile, final String goal) {
 		try {
-			final Pipegraph graph = new Pipegraph(
-					Files.createTempDirectory("test").toFile(),
-					new File("src/test/resources/test.conf"));
+			final Pipegraph graph = new Pipegraph(Files.createTempDirectory("test").toFile(), configFile);
 			final IPipegraphRunner runner = new AsynchronousPipegraphRunner(
 					false);
 			runner.run(graph, Optional.empty());
 			Assert.assertEquals(36,
-					graph.getStage("final").<IntegerResource> readOutput()
+					graph.getStage(goal).<IntegerResource> readOutput()
 							.findFirst().get().getData());
 		} catch (final IOException e) {
 			throw new RuntimeException(e);
